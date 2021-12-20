22dba3e58dbd257e79eb64e9f90aac4660f79d98
Shparki <matt.murray@pnmac.com>
2021-12-20 12:36:01 -0600

    feat(changed): 📝 Updated app.py documenation

--- app.py
+++ app.py
@@ -23,15 +23,20 @@ logging.basicConfig(filename='logs/output.log', level=logging.INFO)
 @click.command()
 @click.option('-url', required=True, help='The URL to crawl.')
 @click.option(
-    '--depth', type=int, default=None, help='The maximumd depth to traverse.'
+    '--depth', type=int, default=None, help='The maximum depth to traverse.'
 )
-def crawl(url: str, depth: int = None):
+@click.option(
+    '--output',
+    default='outputs/sitetree.json',
+    help='The output path to save the sitetree.',
+)
+def crawl(url: str, depth: int, output: str):
     """Crawl a webpage URL with an optional max depth of DEPTH."""
     root = Page(url)
     queue, tree = [root], {}
 
     while queue:
-        current = queue.pop()
+        current = queue.pop(0)
         tree[current.url] = current.to_json()
         print(json.dumps(current.to_json(), indent=2))
 
@@ -45,7 +50,7 @@ def crawl(url: str, depth: int = None):
             ):
                 queue.append(page)
 
-    outputs_path = Path('outputs/sitetree.json').resolve()
+    outputs_path = Path(output).resolve()
     os.makedirs(outputs_path.parent, exist_ok=True)
     with open(outputs_path, mode='w', encoding='utf-8') as buffer:
         json.dump(tree, buffer, indent=2)
